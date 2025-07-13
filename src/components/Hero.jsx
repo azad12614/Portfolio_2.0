507a43bc3d6970c330fa21e0a8b97a9c995543ea
azad12614 <azad.jishan2003@gmail.com>
2025-07-13 22:39:57 +0600

    Hero Section Update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,13 @@
 import React from "react";
 import "./Hero.css";
 import pdf from "../assets/Abdullah_Al_Azad.pdf";
-import avater from "../assets/Abdullah Al Azad.jpg";
+import avater from "../assets/Abdullah_Al_Azad.jpg";
 import logo from "../assets/AZAD.png";
 
 const Hero = () => {
   return (
     <section className="hero-section" id="Hero">
       <div className="heros">
-        <div className="hero-text-box">
-          <h1 className="heading-primary">Abdullah Al Azad</h1>
-          <p className="hero-description">
-            <strong>CSE Student</strong>,{" "}
-            <strong>Competitive Programmer</strong>,{" "}
-            <strong>MERN stack Developer</strong>.
-          </p>
-          <div className="hero-btn">
-            <a href={pdf} target="_blank" className="cv">
-              View Resume
-            </a>
-            <a href="#Project" className="me">
-              Explore Projects
-            </a>
-          </div>
-        </div>
         <div className="hero-img-box">
           <div className="wrapper">
             <img
@@ -38,6 +22,22 @@ const Hero = () => {
               alt="Azad Logo"
               className="hero-img"
             />
+          </div>
+        </div>
+        <div className="hero-text-box">
+          <h1 className="heading-primary">Abdullah Al Azad</h1>
+          <p className="hero-description">
+            <strong>CSE Student</strong>,{" "}
+            <strong>Competitive Programmer</strong>,{" "}
+            <strong>MERN Stack Developer</strong>.
+          </p>
+          <div className="hero-btn">
+            <a href={pdf} target="_blank" className="cv">
+              View Resume
+            </a>
+            <a href="#Project" className="me">
+              Explore Projects
+            </a>
           </div>
         </div>
       </div>
