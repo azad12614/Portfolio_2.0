adae4385edd44130c9a0a8b23459f5542ab00931
azad12614 <azad.jishan2003@gmail.com>
2025-12-18 11:29:59 +0600

    Commit 70

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -12,7 +12,7 @@ import Rating from "../assets/Projects/Rating.png";
 import Sleepyheads from "../assets/Projects/Sleepyheads.png";
 import StartUP from "../assets/Projects/StartUP.png";
 import TaskTracker from "../assets/Projects/Task_Tracker.png";
-import VibeCast from "../assets/Projects/VibeCast.png";
+import VibeCast from "../assets/Projects/Vibe_Cast.png";
 import "./Project.css";
 
 const Project = () => {
